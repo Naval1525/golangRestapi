c33a6e7b8d80795b406f7d059cce829728984248
Naval15 <e23cseu0962@bennett.edu.in>
2025-06-01 14:06:00 +0530

    added gracefull shutdown

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/naval1525/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to students api")
+	// Load config
+	cfg := config.MustLoad()
+	slog.Info("Loaded config: %+v")
+
+	// Database setup (if needed)
+
+	// Router setup
+	router := http.NewServeMux()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			w.Write([]byte("Welcome to server"))
+			return
+		}
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	})
+
+	// Server setup
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server failed: %v", err)
+		}
+	}()
+
+	<-done
+	slog.Info("Shutting down the server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := server.Shutdown(ctx)
+	if err != nil {
+		slog.Error("Server shutdown failed", "error", err)
+	}
+	slog.Info("Server gracefully stopped")
+
 }
